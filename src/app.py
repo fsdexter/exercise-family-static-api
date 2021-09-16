711fd0e7347c48148eceaa82bac3a3f9f4e2648c
fsdexter <fsdexter@gmail.com>
2021-09-16 22:33:15 +0000

    iniciado e 6 testes passados

--- src/app.py
+++ src/app.py
@@ -26,19 +26,134 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_family_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    response_body = members
+    
 
 
     return jsonify(response_body), 200
 
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+
+    # this is how you can use the Family datastructure by calling its methods
+    member = jackson_family.get_member(id)
+    response_body = {
+        "member": member
+    }
+
+    if member is not None:
+        return jsonify(response_body), 200
+    else:
+        return "Member Not FOUND", 400
+
+# ### 3) Add (POST) new member
+
+# Which adds a new member to the family data structure.
+
+# ```md
+# POST /member
+
+# REQUEST BODY (content_type: application/json):
+
+# {
+#     first_name: String,
+#     age: Int,
+#     lucky_numbers: [],
+#     id: Int *optional
+# }
+
+# RESPONSE (content_type: application/json):
+
+# status_code: 200 if success. 400 if bad request (wrong info) screw up, 500 if the server encounter an error
+
+# body: empty
+# ```
+
+# Keep in mind that POST request data dictionary may contain a key and a value for this new member `id`.
+# - If it does not, your API should randomly generate one when adding as a family members.
+# - If it does include it, then that is the value to be used for such end.
+
+
+ 
+        
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
+
+
+
+# @
+# @
+# @
+
+
+# ### 2) Retrieve one member
+
+# Which returns the member of the family where `id == member_id`.
+
+# ```md
+# GET /member/<int:member_id>
+
+# RESPONSE (content_type: application/json):
+
+# status_code: 200 if success. 400 if bad request (wrong info) screw up, 500 if the server encounter an error
+
+# body: //the member's json object
+
+# {
+#     "id": Int,
+#     "first_name": String,
+#     "age": Int,
+#     "lucky_numbers": List
+# }
+
+# ```
+
+# ### 3) Add (POST) new member
+
+# Which adds a new member to the family data structure.
+
+# ```md
+# POST /member
+
+# REQUEST BODY (content_type: application/json):
+
+# {
+#     first_name: String,
+#     age: Int,
+#     lucky_numbers: [],
+#     id: Int *optional
+# }
+
+# RESPONSE (content_type: application/json):
+
+# status_code: 200 if success. 400 if bad request (wrong info) screw up, 500 if the server encounter an error
+
+# body: empty
+# ```
+
+# Keep in mind that POST request data dictionary may contain a key and a value for this new member `id`.
+# - If it does not, your API should randomly generate one when adding as a family members.
+# - If it does include it, then that is the value to be used for such end.
+
+# ### 4) DELETE one member
+
+# Which deletes a family member with `id == member_id`
+
+# ```md
+# DELETE /member/<int:member_id>
+
+# RESPONSE (content_type: application/json):
+
+# status_code: 200 if success. 400 if bad request (wrong info) screw up, 500 if the server encounter an error
+
+# body: {
+#     done: True
+# }    
+
+# ```
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,29 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+            "id" : 3443,
+            "first_name": "John" ,
+            "last_name": "Jackson",
+            "age": 33,
+            "lucky_numbers":[7, 13, 22]
+            },
+            {
+            "id" :2,
+            "first_name": "Jane", 
+            "last_name": "Jackson",
+            "age": 35,  
+            "lucky_numbers":[10, 14, 3]
+            },
+            {
+            "id" :3,
+            "first_name": "Jimmy",
+            "last_name": "Jackson",
+            "age": 5,
+            "lucky_numbers":[1]
+            } 
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -29,7 +51,11 @@ class FamilyStructure:
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+            else:
+                return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
